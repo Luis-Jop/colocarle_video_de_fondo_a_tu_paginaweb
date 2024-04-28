edb74445f79158d33d1ff2f8e8ae67d67c581252
LuisJop <luisjop7@gmail.com>
2024-04-28 13:12:50 -0600

    colocarle estilo a mi proyecto

--- ja.js
+++ ja.js
@@ -1,18 +1,18 @@
-const week = [ "Monday", "Tuesday","Wednesday ","Thursday","Friday","saturday","sunday"]
+const week = [ "sun","Mon", "Tues","Wed ","Thur","Fri","satu"]
  
 
 function updatetime() {
     var now = new Date();
 
     document.getElementById("time").innerText =
-            zeropadding(now.getHours(), 2)+";"+
-            zeropadding(now.getMinutes(),2)+ ";"+
-            zeropadding(now.getSeconds(),2);
+            zeropadding(now.getHours(), 2)+":"+
+            zeropadding(now.getMinutes(),2)+ ":"+
+            zeropadding(now.getSeconds(),2) ;
 
     document.getElementById("date").innerText =
              now.getFullYear()+ "/"+
              zeropadding(now.getMonth()+ 1 , 2)+ "/"+
-             zeropadding(now.getDate(),2)+ "__"+
+             zeropadding(now.getDate(),2)+ ""+
              week[now.getDay()];
     
     
